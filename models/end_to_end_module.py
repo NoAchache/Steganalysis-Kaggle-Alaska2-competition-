a4e62f708a516b16fb6019b0e9139ed7cacaa897
noe <noe.achache@gmail.com>
2020-08-02 16:37:25 +0200

    change of net

--- models/end_to_end_module.py
+++ models/end_to_end_module.py
@@ -1,5 +1,7 @@
 import torch.nn as nn
 import torch
+# https://github.com/lukemelas/EfficientNet-PyTorch
+from efficientnet_pytorch import EfficientNet
 
 from models.network import SrmNetwork, ClassificationNetwork
 
@@ -8,7 +10,8 @@ class EndToEndNet(nn.Module):
     def __init__(self):
         super(EndToEndNet, self).__init__()
         self.srm = SrmNetwork()
-        self.efficient_net = ClassificationNetwork(in_channels=30, num_classes=4)
+        self.efficient_net = EfficientNet.from_pretrained('efficientnet-b2', in_channels=30)
+        self.efficient_net._fc = nn.Linear(in_features=1408, out_features=4, bias=True)
         self.log_softmax = torch.nn.LogSoftmax(dim=-1)
     def forward(self, img_input):
         srm_image = self.srm(img_input)
